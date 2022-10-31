70a686e6c2b3928d606477268ebdc8b61210d631
tglima <thg.limadesousa@gmail.com>
2022-10-31 06:27:01 -0300

    Substitui a chamado do checkAuth por checkAuthDb

--- app/controllers/jwt.js
+++ app/controllers/jwt.js
@@ -1,6 +1,6 @@
 const jwtService = require('../services/jwt');
 
 exports.auth = async function (req, res) {
-  const response = await jwtService.checkAuth(req);
+  const response = await jwtService.checkAuthDb(req.body);
   return res.status(response.statusCode).send(response.jsonBody);
 };
