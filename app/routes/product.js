74dec1d1e6c6564e7033d9ceddf854c3d16da091
tglima <thg.limadesousa@gmail.com>
2022-11-02 20:38:00 -0300

    Protege a rota de produtos

--- app/routes/product.js
+++ app/routes/product.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/product');
+const jwtService = require('../services/jwt');
 
-router.get('/find', productController.findAll);
-router.get('/find/id=:id', productController.findById);
-router.post('', productController.saveNew);
+router.get('/find', jwtService.verifyJWT, productController.findAll);
+router.get('/find/id=:id', jwtService.verifyJWT, productController.findById);
+router.post('', jwtService.verifyJWT, productController.saveNew);
 module.exports = router;
