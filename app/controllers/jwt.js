1bd1789a79ae723dc404455ee8887be18be21d05
tglima <thg.limadesousa@gmail.com>
2022-11-02 15:57:00 -0300

    Simplifica o exports auth

--- app/controllers/jwt.js
+++ app/controllers/jwt.js
@@ -1,6 +1,6 @@
 const jwtService = require('../services/jwt');
 
-exports.auth = async function (req, res) {
+exports.auth = async (req, res) => {
   const response = await jwtService.checkAuthDb(req.body);
   return res.status(response.statusCode).send(response.jsonBody);
 };
