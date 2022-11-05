794b2a33d3ee2c8c628f7767216f433971e66626
tglima <thg.limadesousa@gmail.com>
2022-11-05 19:47:53 -0300

    Adiciona novas constantes

---
+++ app/helpers/constants.js
@@ -0,0 +1,7 @@
+exports.HTTP_MSG_ERROR_200 = 'OK';
+exports.HTTP_MSG_ERROR_201 = 'CREATED';
+exports.HTTP_MSG_ERROR_400 = 'Bad Request';
+exports.HTTP_MSG_ERROR_401 = 'Invalid credentials';
+exports.HTTP_MSG_ERROR_401_UWP = 'User without permission';
+exports.HTTP_MSG_ERROR_404 = 'Not Found';
+exports.HTTP_MSG_ERROR_500 = 'Internal Server Error';
