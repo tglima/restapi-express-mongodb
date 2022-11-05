89eb3ec68e430bd0302570a5503bb8f22be42982
tglima <thg.limadesousa@gmail.com>
2022-11-05 14:22:26 -0300

    Ajustes e melhorias no JwtService

--- app/services/jwt.js
+++ app/services/jwt.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken');
 const appConfig = require('../config/app.config');
 const User = require('../models/User');
 const AccessRole = require('../models/acessRole');
+const msgUserWithoutPermission = 'User without permission';
+const msgInvalidCredentials = 'Invalid credentials';
 
 exports.checkAuthDb = async (reqBody) => {
   const response = {
@@ -46,29 +48,35 @@ exports.verifyJWT = async (req, res, next) => {
   const bearerHeader = req.headers.authorization.replace('Bearer ', '');
 
   if (req.headers.authorization === undefined) {
-    return res.status(401).json({ success: false, message: 'Invalid credentials' });
+    return res.status(401).json({ success: false, message: msgInvalidCredentials });
   }
 
   if (!bearerHeader) {
-    return res.status(401).json({ success: false, message: 'Invalid credentials' });
+    return res.status(401).json({ success: false, message: msgInvalidCredentials });
   }
+
   const userDataReq = await this.getUserDataReq(req);
 
   let acessRole;
+
   try {
-    acessRole = await AccessRole.findOne({ endpoint: req.originalUrl, idRole: userDataReq.idRole });
+
+    if (userDataReq.idRole === appConfig.superUser.idRole) {
+      return next();
+    }
+
+    acessRole = await AccessRole.findOne(
+      { endpoint: req.originalUrl, idRole: userDataReq.idRole },
+    );
 
     if (acessRole === null || acessRole === undefined) {
-      return res.status(401).json({ success: false, message: 'Invalid token' });
+      return res.status(401).json({ success: false, message: msgUserWithoutPermission });
     }
+
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
-    return res.status(401).json({ success: false, message: 'Invalid token' });
-  }
-
-  if (acessRole.idRole === appConfig.superUser.idRole) {
-    return next();
+    return res.status(401).json({ success: false, message: msgUserWithoutPermission });
   }
 
   const nmMethod = req.method.toString();
@@ -81,7 +89,7 @@ exports.verifyJWT = async (req, res, next) => {
     return next();
   }
 
-  return res.status(401).json({ success: false, message: 'User without permission' });
+  return res.status(401).json({ success: false, message: msgUserWithoutPermission });
 };
 
 exports.getUserDataReq = async (req) => {
