e7ffa59c298607de01dcbbd97bca2fa816756d46
tglima <thg.limadesousa@gmail.com>
2022-11-05 19:48:39 -0300

    Adiciona novo métod SaveCustomer

--- app/services/customer.js
+++ app/services/customer.js
@@ -1,4 +1,6 @@
 const Customer = require('../models/Customer');
+const Constant = require('../helpers/constants');
+const jwtService = require('./jwt');
 
 exports.FindById = async (req) => {
 
@@ -87,6 +89,50 @@ exports.FindByNuDocument = async (req) => {
     response.statusCode = 500;
     response.success = false;
     response.jsonBody = 'Internal Server Error';
+
+  }
+
+  return response;
+
+};
+
+exports.SaveCustomer = async (req) => {
+
+  const response = {
+    statusCode: 400, success: false, jsonBody: Constant.HTTP_MSG_ERROR_400,
+  };
+
+  try {
+
+    const customer = req.body;
+
+    customer.idUserRegister = (await jwtService.getUserDataReq(req)).idUserRegister;
+
+    const customerDb = await Customer.findOne({ nuDocument: customer.nuDocument, isActive: true });
+
+    if (customerDb !== undefined && customerDb !== null) {
+
+      response.jsonBody = 'O número do documento informado, já existe!';
+      response.wasSuccess = false;
+      return response;
+
+    }
+
+    const customerDB = await Customer.create(customer);
+
+    response.statusCode = 201;
+    response.success = true;
+    response.jsonBody = {
+      id: customerDB.id, nmCustomer: customer.nmCustomer, nuDocument: customer.nuDocument,
+    };
+
+  } catch (error) {
+
+    // eslint-disable-next-line no-console
+    console.error(error);
+    response.statusCode = 500;
+    response.success = false;
+    response.jsonBody = Constant.HTTP_MSG_ERROR_500;
 
   }
 
