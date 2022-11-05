854e486ba2f8673a82aa72138f8ec402160b6d34
tglima <thg.limadesousa@gmail.com>
2022-11-05 19:50:04 -0300

    Adiciona nova rota /save

--- app/routes/customer.js
+++ app/routes/customer.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const customerController = require('../controllers/customer');
 const jwtService = require('../services/jwt');
 
+router.post('/save', jwtService.verifyJWT, customerController.saveCustomer);
 router.get('/find/id=:id', jwtService.verifyJWT, customerController.findById);
 router.get('/find/nuDocument=:nuDocument', jwtService.verifyJWT, customerController.findByNuDocument);
 module.exports = router;
