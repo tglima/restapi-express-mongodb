9810b09b5f086a93a8b5eec1dd99254076044958
tglima <thg.limadesousa@gmail.com>
2022-11-13 16:53:00 -0300

    Corrigi o nome dos métodos e add o updateCustomer

--- app/controllers/customer.js
+++ app/controllers/customer.js
@@ -2,21 +2,28 @@ const customerService = require('../services/customer');
 
 exports.findById = async (req, res) => {
 
-  const response = await customerService.FindById(req);
+  const response = await customerService.findById(req);
   return res.status(response.statusCode).send(response.jsonBody);
 
 };
 
 exports.findByNuDocument = async (req, res) => {
 
-  const response = await customerService.FindByNuDocument(req);
+  const response = await customerService.findByNuDocument(req);
   return res.status(response.statusCode).send(response.jsonBody);
 
 };
 
 exports.saveCustomer = async (req, res) => {
 
-  const response = await customerService.SaveCustomer(req);
+  const response = await customerService.saveCustomer(req);
+  return res.status(response.statusCode).send(response.jsonBody);
+
+};
+
+exports.updateCustomer = async (req, res) => {
+
+  const response = await customerService.updateCustomer(req);
   return res.status(response.statusCode).send(response.jsonBody);
 
 };
