e0ccd7593c91682d87607952bf94942ea53eb444
tglima <thg.limadesousa@gmail.com>
2022-11-13 16:51:50 -0300

    Refatora CustomerServicee adiciona novos métodos

--- app/services/customer.js
+++ app/services/customer.js
@@ -1,9 +1,9 @@
-const Customer = require('../models/Customer');
+const customerModel = require('../models/Customer');
 const constant = require('../helpers/constants');
 const validatorCustomer = require('../validators/customer');
 const jwtService = require('./jwt');
 
-exports.FindById = async (req) => {
+exports.findById = async (req) => {
 
   const response = {
     statusCode: 404, success: false, jsonBody: 'Not Found',
@@ -13,47 +13,42 @@ exports.FindById = async (req) => {
 
   if (idCustomer === null || idCustomer === undefined) {
 
-    response.statusCode = 400;
+    response.statusCode = constant.HTTP_MSG_ERROR_400;
     response.success = false;
-    response.jsonBody = 'Uninformed id';
+    response.jsonBody = 'id Não informado!';
     return response;
 
   }
 
-  let customer;
+  const result = await customerModel.findByIdCustomer(idCustomer);
 
-  try {
+  if (!result.wasSuccess) {
 
-    customer = await Customer.findById({ _id: idCustomer, isActive: true });
-
-    if (customer === undefined || customer === null) {
-
-      return response;
-
-    }
-
-    response.statusCode = 200;
-    response.success = true;
-    response.jsonBody = customer;
-
-  } catch (error) {
-
-    // eslint-disable-next-line no-console
-    console.error(error);
     response.statusCode = 500;
     response.success = false;
-    response.jsonBody = 'Internal Server Error';
+    response.jsonBody = constant.HTTP_MSG_ERROR_500;
+    return response;
 
   }
+
+  if (result.customer === undefined || result.customer === null) {
+
+    return response;
+
+  }
+
+  response.statusCode = 200;
+  response.success = true;
+  response.jsonBody = result.customer;
 
   return response;
 
 };
 
-exports.FindByNuDocument = async (req) => {
+exports.findByNuDocument = async (req) => {
 
   const response = {
-    statusCode: 404, success: false, jsonBody: 'Not Found',
+    statusCode: 404, success: false, jsonBody: constant.HTTP_MSG_ERROR_404,
   };
 
   const { nuDocument } = req.params;
@@ -62,95 +57,184 @@ exports.FindByNuDocument = async (req) => {
 
     response.statusCode = 400;
     response.success = false;
-    response.jsonBody = 'Uninformed nuDocument';
+    response.jsonBody = 'nuDocument não informado!';
     return response;
 
   }
 
-  let customer;
+  const result = await customerModel.findByNuDocument(nuDocument);
 
-  try {
+  if (!result.wasSuccess) {
 
-    customer = await Customer.findOne({ nuDocument, isActive: true });
-
-    if (customer === undefined || customer === null) {
-
-      return response;
-
-    }
-
-    response.statusCode = 200;
-    response.success = true;
-    response.jsonBody = customer;
-
-  } catch (error) {
-
-    // eslint-disable-next-line no-console
-    console.error(error);
     response.statusCode = 500;
     response.success = false;
-    response.jsonBody = 'Internal Server Error';
+    response.jsonBody = constant.HTTP_MSG_ERROR_500;
+    return response;
 
   }
+
+  if (result.customer === undefined || result.customer === null) {
+
+    return response;
+
+  }
+
+  response.statusCode = 200;
+  response.success = true;
+  response.jsonBody = result.customer;
 
   return response;
 
 };
 
-exports.SaveCustomer = async (req) => {
+exports.saveCustomer = async (req) => {
 
   const response = {
     statusCode: 400, success: false, jsonBody: constant.HTTP_MSG_ERROR_400,
   };
 
   let returnValidate = { wasSuccess: true, messages: [] };
+  const customer = req.body;
 
-  try {
+  customer.idUserRegister = (await jwtService.getUserDataReq(req)).id;
+  customer.idLastUserEdit = (await jwtService.getUserDataReq(req)).id;
+  returnValidate = await validatorCustomer.validateSaveCustomer(customer);
 
-    const customer = req.body;
+  if (!returnValidate.wasSuccess) {
 
-    customer.idUserRegister = (await jwtService.getUserDataReq(req)).id;
-    customer.idLastUserEdit = (await jwtService.getUserDataReq(req)).id;
-    returnValidate = await validatorCustomer.validateSaveCustomer(customer);
+    response.statusCode = 400;
+    response.success = false;
+    response.jsonBody = JSON.parse(JSON.stringify(returnValidate.messages));
+    return response;
 
-    if (!returnValidate.wasSuccess) {
+  }
 
-      response.statusCode = 400;
-      response.success = false;
-      response.jsonBody = JSON.parse(JSON.stringify(returnValidate.messages));
-      return response;
+  const result = await customerModel.findByNuDocument(customer.nuDocument);
 
-    }
+  if (!result.wasSuccess) {
 
-    const customerDb = await Customer.findOne({ nuDocument: customer.nuDocument, isActive: true });
-
-    if (customerDb !== undefined && customerDb !== null) {
-
-      response.statusCode = 400;
-      response.success = false;
-      returnValidate.messages.push('O número do documento informado, já existe!');
-      response.jsonBody = JSON.parse(JSON.stringify(returnValidate.messages));
-      return response;
-
-    }
-
-    const customerDB = await Customer.create(customer);
-
-    response.statusCode = 201;
-    response.success = true;
-    response.jsonBody = {
-      id: customerDB.id, nmCustomer: customer.nmCustomer, nuDocument: customer.nuDocument,
-    };
-
-  } catch (error) {
-
-    // eslint-disable-next-line no-console
-    console.error(error);
     response.statusCode = 500;
     response.success = false;
     response.jsonBody = constant.HTTP_MSG_ERROR_500;
+    return response;
 
   }
+
+  if (result.customer !== undefined && result.customer !== null) {
+
+    response.statusCode = 400;
+    response.success = false;
+    returnValidate.messages.push('O número do documento informado, já existe!');
+    response.jsonBody = JSON.parse(JSON.stringify(returnValidate.messages));
+    return response;
+
+  }
+
+  const resultSave = await customerModel.saveNew(customer);
+
+  if (!resultSave.wasSuccess) {
+
+    response.statusCode = 500;
+    response.success = false;
+    response.jsonBody = constant.HTTP_MSG_ERROR_500;
+    return response;
+
+  }
+
+  response.statusCode = 201;
+  response.success = true;
+  response.jsonBody = resultSave.customer;
+
+  return response;
+
+};
+
+exports.updateCustomer = async (req) => {
+
+  const response = {
+    statusCode: 400,
+    success: false,
+    jsonBody: constant.HTTP_MSG_ERROR_400,
+  };
+
+  const idCustomer = req.params.id;
+  const customer = req.body;
+  customer.id = idCustomer;
+
+  if (idCustomer === null || idCustomer === undefined) {
+
+    response.statusCode = 400;
+    response.success = false;
+    response.jsonBody = 'id Não informado!';
+    return response;
+
+  }
+
+  let returnValidate = { wasSuccess: true, messages: [] };
+  returnValidate = await validatorCustomer.validateSaveCustomer(customer);
+
+  if (!returnValidate.wasSuccess) {
+
+    response.statusCode = 400;
+    response.success = false;
+    response.jsonBody = JSON.parse(JSON.stringify(returnValidate.messages));
+    return response;
+
+  }
+
+  const resultFind = await customerModel.findByIdCustomer(idCustomer);
+
+  if (!resultFind.wasSuccess) {
+
+    response.statusCode = 500;
+    response.success = false;
+    response.jsonBody = constant.HTTP_MSG_ERROR_500;
+    return response;
+
+  }
+
+  if (resultFind.customer === null || resultFind.customer === undefined) {
+
+    return response;
+
+  }
+
+  const resultFindDocument = await customerModel.findByNuDocument(customer.nuDocument);
+
+  if (!resultFindDocument.wasSuccess) {
+
+    response.statusCode = 500;
+    response.success = false;
+    response.jsonBody = constant.HTTP_MSG_ERROR_500;
+    return response;
+
+  }
+
+  if ((resultFindDocument.customer !== null || resultFindDocument.customer !== undefined)
+    && resultFindDocument.customer.id !== customer.id) {
+
+    response.statusCode = 400;
+    response.success = false;
+    response.jsonBody = 'O documento informado já pertence a outro cliente!';
+    return response;
+
+  }
+
+  customer.idLastUserEdit = (await jwtService.getUserDataReq(req)).id;
+  const resultUpdate = await customerModel.updateCustomer(customer);
+
+  if (!resultUpdate.wasSuccess) {
+
+    response.statusCode = 500;
+    response.success = false;
+    response.jsonBody = constant.HTTP_MSG_ERROR_500;
+    return response;
+
+  }
+
+  response.statusCode = 200;
+  response.success = true;
+  response.jsonBody = resultUpdate.customer;
 
   return response;
 
