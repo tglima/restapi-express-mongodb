0a22bcf859b10b1b249740911b7acf478d448010
tglima <thg.limadesousa@gmail.com>
2022-11-13 07:06:59 -0300

    Altera o model Product

--- app/models/Product.js
+++ app/models/Product.js
@@ -1,16 +1,36 @@
 const mongoose = require('mongoose');
 
-const Product = mongoose.model('products', {
-  nmProduct: { type: String, required: true },
-  vlMonthPrice: { type: Number, min: 0, required: true },
-  details: {
-    nmVideoQuality: { type: String, required: true },
-    nmResolution: { type: String, required: true },
-    qtSimultaneousScreens: { type: Number, min: 0, required: true },
+const schema = new mongoose.Schema(
+  {
+    nmProduct: { type: String, required: true },
+    vlMonthPrice: { type: Number, min: 0, required: true },
+    details: {
+      nmVideoQuality: { type: String, required: true },
+      nmResolution: { type: String, required: true },
+      qtSimultaneousScreens: { type: Number, min: 0, required: true },
+    },
+    idUserRegister: { type: Number, min: 2, required: true },
+    dtRegister: { type: Date, default: new Date().toJSON() },
+    isActive: { type: Boolean, default: true },
   },
-  idUserRegister: { type: Number, min: 2, required: true },
-  dtRegister: { type: Date, default: new Date().toJSON() },
-  isActive: { type: Boolean, default: true },
-});
+  { versionKey: false },
+);
 
+schema.methods.toJSON = function toJSON() {
+
+  const prodJSON = {};
+  const obj = this.toObject();
+
+  // eslint-disable-next-line no-underscore-dangle
+  prodJSON.id = obj._id;
+  prodJSON.nmProduct = obj.nmProduct;
+  prodJSON.vlMonthPrice = obj.vlMonthPrice;
+  prodJSON.nuDocument = obj.nuDocument;
+  prodJSON.details = obj.details;
+
+  return prodJSON;
+
+};
+
+const Product = mongoose.model('products', schema);
 module.exports = Product;
