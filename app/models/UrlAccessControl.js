715a0d3a4cc20413784b6e87e06cb48bb5b388a6
tglima <thg.limadesousa@gmail.com>
2022-11-17 10:04:03 -0300

    Refatora URLAccessControl

--- app/models/UrlAccessControl.js
+++ app/models/UrlAccessControl.js
@@ -1,36 +1,56 @@
 const mongoose = require('mongoose');
+const schema = new mongoose.Schema(
+  {
+    url: { type: String, required: true },
+    isActive: { type: Boolean, default: false },
+    post: {
+      idRolesAllowed: {
+        type: Array,
+        required: true,
+        default: [],
+      },
+    },
+    update: {
+      idRolesAllowed: {
+        type: Array,
+        required: true,
+        default: [],
+      },
+    },
+    read: {
+      idRolesAllowed: {
+        type: Array,
+        required: true,
+        default: [],
+      },
+    },
+    delete: {
+      idRolesAllowed: {
+        type: Array,
+        required: true,
+        default: [],
+      },
+    },
+  },
+);
 
-const URLAccessControl = mongoose.model('urlAccessControl', {
-  url: { type: String, required: true },
-  isActive: { type: Boolean, default: false },
-  post: {
-    idRolesAllowed: {
-      type: Array,
-      required: true,
-      default: [],
-    },
-  },
-  update: {
-    idRolesAllowed: {
-      type: Array,
-      required: true,
-      default: [],
-    },
-  },
-  read: {
-    idRolesAllowed: {
-      type: Array,
-      required: true,
-      default: [],
-    },
-  },
-  delete: {
-    idRolesAllowed: {
-      type: Array,
-      required: true,
-      default: [],
-    },
-  },
-}, 'urlAccessControl');
+const URLAccessControl = mongoose.model('urlAccessControl', schema, 'urlAccessControl');
 
-module.exports = URLAccessControl;
+exports.findByUrlBase = async (urlBase) => {
+
+  const result = { wasSuccess: false, urlAccessControl: undefined, error: null };
+
+  try {
+
+    result.urlAccessControl = await URLAccessControl.findOne({ url: urlBase, isActive: true });
+    result.wasSuccess = true;
+
+  } catch (error) {
+
+    result.error = error;
+
+  }
+
+  return result;
+
+};
