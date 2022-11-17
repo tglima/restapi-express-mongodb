697b336bae4838a56e96fae890d008ef5f49fe5e
tglima <thg.limadesousa@gmail.com>
2022-11-17 09:37:00 -0300

    Refatora a UserModel

--- app/models/User.js
+++ app/models/User.js
@@ -1,15 +1,45 @@
 const mongoose = require('mongoose');
 
-const User = mongoose.model('users', {
-  idUser: {
-    type: Number, min: 2, required: true, unique: true,
+const schema = new mongoose.Schema(
+  {
+    idUser: {
+      type: Number, min: 2, required: true, unique: true,
+    },
+    nmUser: { type: String, required: true },
+    deUserName: { type: String, required: true },
+    dePassword: { type: String, required: true },
+    dtRegister: { type: Date, default: new Date().toJSON() },
+    idRole: { type: Number, min: 2, required: true },
+    isActive: { type: Boolean, default: true },
   },
-  nmUser: { type: String, required: true },
-  deUserName: { type: String, required: true },
-  dePassword: { type: String, required: true },
-  dtRegister: { type: Date, default: new Date().toJSON() },
-  idRole: { type: Number, min: 2, required: true },
-  isActive: { type: Boolean, default: true },
-});
+);
 
-module.exports = User;
+const User = mongoose.model('users', schema);
+
+exports.findByUsernameAndPass = async (username, password) => {
+
+  const result = { wasSuccess: false, user: null, error: null };
+
+  try {
+
+    result.user = await User.findOne(
+      {
+        deUserName: username,
+        dePassword: password,
+        isActive: true,
+      },
+    );
+
+    result.wasSuccess = true;
+
+  } catch (error) {
+
+    result.user = null;
+    result.wasSuccess = false;
+    result.error = error;
+
+  }
+
+  return result;
+
+};
