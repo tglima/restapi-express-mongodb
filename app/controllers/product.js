6beb78d7eb714bcd8489268b34abc9f3401ee60a
tglima <thg.limadesousa@gmail.com>
2022-11-17 15:03:33 -0300

    Corrigi o nome dos métodos

--- app/controllers/product.js
+++ app/controllers/product.js
@@ -2,14 +2,14 @@ const productService = require('../services/product');
 
 exports.findAll = async (req, res) => {
 
-  const response = await productService.FindAll();
+  const response = await productService.findAll();
   return res.status(response.statusCode).send(response.jsonBody);
 
 };
 
 exports.findById = async (req, res) => {
 
-  const response = await productService.FindById(req);
+  const response = await productService.findById(req);
   return res.status(response.statusCode).send(response.jsonBody);
 
 };
