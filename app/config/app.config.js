f5766fab5fd94bdd41bb65547184ce2bd2fbb64f
tglima <thg.limadesousa@gmail.com>
2022-11-21 07:29:48 -0300

    Melhoria em algumas props e adição de nova

--- app/config/app.config.js
+++ app/config/app.config.js
@@ -6,15 +6,16 @@ const appConfig = {
     tokenType: process.env.TOKEN_TYPE,
   },
   superUser: {
-    idUser: process.env.ID_SUPER_USER, idRole: process.env.ID_SUPER_ROLE,
+    idUserRegister: process.env.ID_SUPER_USER, idRole: process.env.ID_SUPER_ROLE,
   },
   guestUser: {
-    idUser: process.env.ID_GUEST_USER, idRole: process.env.ID_GUEST_ROLE, id: undefined,
+    idUserRegister: process.env.ID_GUEST_USER, idRole: process.env.ID_GUEST_ROLE,
   },
   nuVersionApi: process.env.API_NUVERSION,
   serverPort: process.env.SERVER_PORT,
   dbConnection: `${process.env.DB_PREFIX_CONN}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_SERVER}/${process.env.DB_NAME}`,
-  qtMinCharMessage: process.env.QT_MIN_CHAR_MESSAGE,
+  qtMinCharMessage: Number(process.env.QT_MIN_CHAR_MESSAGE),
+  nuYearsValProduct: Number(process.env.NU_YEARS_VAL_PRODUCT),
 };
 
 module.exports = appConfig;
