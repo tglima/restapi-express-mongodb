f185cf3da0a752f7221d8fff2e9d0e31b9411ec3
tglima <thg.limadesousa@gmail.com>
2022-11-21 16:26:40 -0300

    Ajustes e simplificação no UserModel

--- app/models/User.js
+++ app/models/User.js
@@ -2,19 +2,31 @@ const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema(
   {
-    idUser: {
-      type: Number, min: 2, required: true, unique: true,
-    },
     nmUser: { type: String, required: true },
     deUserName: { type: String, required: true },
     dePassword: { type: String, required: true },
-    dtRegister: { type: Date, default: new Date().toJSON() },
     idRole: { type: Number, min: 2, required: true },
     isActive: { type: Boolean, default: true },
   },
 );
 
-const User = mongoose.model('users', schema);
+schema.methods.toJSON = function toJSON() {
+
+  const userJSON = {};
+  const obj = this.toObject();
+
+  userJSON.id = obj.id;
+  userJSON.nmUser = obj.nmUser;
+  userJSON.deUserName = obj.deUserName;
+  userJSON.dePassword = obj.dePassword;
+  userJSON.nmProduct = obj.nmProduct;
+  userJSON.idRole = obj.idRole;
+  userJSON.isActive = obj.isActive;
+  return userJSON;
+
+};
+
+const User = mongoose.model('users', schema, 'users');
 
 exports.findByUsernameAndPass = async (username, password) => {
 
@@ -30,7 +42,7 @@ exports.findByUsernameAndPass = async (username, password) => {
       },
     );
 
-    result.user = result.user == null ? undefined : result.user;
+    result.user = result.user === null ? undefined : result.user;
     result.wasSuccess = true;
 
   } catch (error) {
