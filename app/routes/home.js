cd6f7d091dc27f596b94ef95d1ddc26948cf683e
tglima <thg.limadesousa@gmail.com>
2022-11-22 11:44:09 -0300

    Simplifca  retornando sempre o status 404

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const homeController = require('../controllers/home');
 const router = express.Router();
-const jwtService = require('../services/jwt');
 
-router.get('/', jwtService.verifyJWT, homeController.getHome);
+router.get('/', homeController.getHome);
+router.post('/', homeController.getHome);
+router.put('/', homeController.getHome);
+router.delete('/', homeController.getHome);
 module.exports = router;
