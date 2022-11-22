a5fc0f3edaa4827f46c6a08b7de6c3503c57fa11
tglima <thg.limadesousa@gmail.com>
2022-11-22 10:26:41 -0300

    Adiciona o service logService

--- app/controllers/jwt.js
+++ app/controllers/jwt.js
@@ -1,8 +1,11 @@
 const jwtService = require('../services/jwt');
+const logService = require('../services/apiLog');
 
 exports.auth = async (req, res) => {
 
+  const dtStart = new Date().toJSON();
   const response = await jwtService.checkAuthDb(req.body);
+  await logService.saveLogDB(req, response, dtStart);
   return res.status(response.statusCode).send(response.jsonBody);
 
 };
