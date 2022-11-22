7a097e9449a2b0a9e074de8fe30eaff46cf3090b
tglima <thg.limadesousa@gmail.com>
2022-11-22 12:06:49 -0300

    Alterado para salvar o log

--- app/controllers/order.js
+++ app/controllers/order.js
@@ -1,36 +1,62 @@
 const orderService = require('../services/order');
+const logService = require('../services/apiLog');
 
 exports.saveOrder = async (req, res) => {
 
+  const dtStart = new Date().toJSON();
+
   const response = await orderService.saveNewOrder(req);
+
+  await logService.saveLogDB(req, response, dtStart);
+
   return res.status(response.statusCode).send(response.jsonBody);
 
 };
 
 exports.findByNuDocument = async (req, res) => {
 
+  const dtStart = new Date().toJSON();
+
   const response = await orderService.findOrderByNuDocumentCustomer(req);
+
+  await logService.saveLogDB(req, response, dtStart);
+
   return res.status(response.statusCode).send(response.jsonBody);
 
 };
 
 exports.findByIdCustomer = async (req, res) => {
 
+  const dtStart = new Date().toJSON();
+
   const response = await orderService.findOrderByIdCustomer(req);
+
+  await logService.saveLogDB(req, response, dtStart);
+
   return res.status(response.statusCode).send(response.jsonBody);
 
 };
 
 exports.updateOrder = async (req, res) => {
 
+  const dtStart = new Date().toJSON();
+
   const response = await orderService.updateOrder(req);
+
+  await logService.saveLogDB(req, response, dtStart);
+
   return res.status(response.statusCode).send(response.jsonBody);
 
 };
 
 exports.cancelOrder = async (req, res) => {
 
+  const dtStart = new Date().toJSON();
+
   const response = await orderService.findAndCancelOrder(req);
+
+  await logService.saveLogDB(req, response, dtStart);
+
   return res.status(response.statusCode).send(response.jsonBody);
 
 };
