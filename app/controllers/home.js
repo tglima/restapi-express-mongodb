2f749905c8804f94814784162bf83c422de0068b
tglima <thg.limadesousa@gmail.com>
2022-11-22 11:27:31 -0300

    Melhoria no homeController Salvar o log no banco de dados e sempre retornar o status 404

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,4 +1,11 @@
-exports.getHome = (req, res) => res.status(200).send({
-  success: true,
-  message: 'OK',
-});
+const logService = require('../services/apiLog');
+const constant = require('../helpers/constants');
+
+exports.getHome = async (req, res) => {
+
+  const dtStart = new Date().toJSON();
+  const response = { statusCode: 404, jsonBody: constant.HTTP_MSG_ERROR_404 };
+  await logService.saveLogDB(req, response, dtStart);
+  return res.status(response.statusCode).send(response.jsonBody);
+
+};
