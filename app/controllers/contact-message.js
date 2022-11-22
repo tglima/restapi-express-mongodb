37bf702ef349639534ef64f165ede5eac88a4489
tglima <thg.limadesousa@gmail.com>
2022-11-22 11:50:45 -0300

    Alterado para salvar o lgo

--- app/controllers/contact-message.js
+++ app/controllers/contact-message.js
@@ -1,8 +1,11 @@
 const contactMessageService = require('../services/contact-message');
+const logService = require('../services/apiLog');
 
 exports.save = async (req, res) => {
 
+  const dtStart = new Date().toJSON();
   const response = await contactMessageService.saveMessage(req);
+  await logService.saveLogDB(req, response, dtStart);
   return res.status(response.statusCode).send(response.jsonBody);
 
 };
