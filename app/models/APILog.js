58e8812d190516b1bf5bc36c6d2c85188faa1c12
tglima <thg.limadesousa@gmail.com>
2022-11-22 10:23:29 -0300

    Adiciona o model APILog

---
+++ app/models/APILog.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema(
+  {
+    url: { type: String, required: true },
+    idUserRegister: { type: String },
+    reqHeaders: { type: Object },
+    reqParams: { type: Object },
+    reqBody: { type: Object },
+    reqMethod: { type: String },
+    resStatusCode: { type: String },
+    resJsonBody: { type: Object },
+    error: { type: Object },
+    dtStart: { type: Date, default: new Date().toJSON() },
+    dtFinish: { type: Date, default: new Date().toJSON() },
+  },
+  { versionKey: false },
+);
+
+const Log = mongoose.model('apiLogs', schema, 'apiLogs');
+
+exports.saveNew = async (log) => {
+
+  const result = { wasSuccess: false, logDB: undefined, error: null };
+
+  try {
+
+    result.logDB = await Log.create(log);
+    result.logDB = result.logDB === null ? undefined : result.logDB;
+    result.wasSuccess = true;
+
+  } catch (error) {
+
+    result.logDB = undefined;
+    result.wasSuccess = false;
+    result.error = error;
+
+  }
+
+  return result;
+
+};
