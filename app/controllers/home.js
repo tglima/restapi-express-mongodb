0802e786342fb027d3cf95663cf1fe736e5db6d7
tglima <thg.limadesousa@gmail.com>
2022-11-22 14:53:46 -0300

    Atualiza para utilizar a constante

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -4,7 +4,7 @@ const constant = require('../helpers/constants');
 exports.getHome = async (req, res) => {
 
   const dtStart = new Date().toJSON();
-  const response = { statusCode: 404, jsonBody: constant.HTTP_MSG_ERROR_404 };
+  const response = constant.RESULT_DEF_ERROR_404;
   await logService.saveLogDB(req, response, dtStart);
   return res.status(response.statusCode).send(response.jsonBody);
 
