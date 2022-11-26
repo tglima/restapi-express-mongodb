432a3d8e7c259fd40f82935fb54bcef49c8433e0
tglima <thg.limadesousa@gmail.com>
2022-11-26 12:10:59 -0300

    Remove o controller e refatora o route Atribui  as responsabilidades do controller para o route

--- app/routes/product.js
+++ app/routes/product.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const productController = require('../controllers/product');
 const jwtService = require('../services/jwt');
+const productService = require('../services/product');
+const logService = require('../services/apiLog');
 
-router.get('/find', jwtService.verifyJWT, productController.findAll);
-router.get('/find/id=:id', jwtService.verifyJWT, productController.findById);
+const findAll = async (req, res) => {
+
+  const dtStart = new Date().toJSON();
+
+  const response = await productService.findAll();
+
+  await logService.saveLogDB(req, response, dtStart);
+
+  return res.status(response.statusCode).send(response.jsonBody);
+
+};
+
+const findById = async (req, res) => {
+
+  const dtStart = new Date().toJSON();
+
+  const response = await productService.findById(req);
+
+  await logService.saveLogDB(req, response, dtStart);
+
+  return res.status(response.statusCode).send(response.jsonBody);
+
+};
+
+router.get('/find', jwtService.verifyJWT, findAll);
+router.get('/find/id=:id', jwtService.verifyJWT, findById);
 module.exports = router;
