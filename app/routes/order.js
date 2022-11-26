c91a18b806ec1c5979be89d98f1c89724f717a0a
tglima <thg.limadesousa@gmail.com>
2022-11-26 12:19:59 -0300

    Remove o controller e refatora o route Atribui  as responsabilidades do controller para o route

--- app/routes/order.js
+++ app/routes/order.js
@@ -1,11 +1,72 @@
 const express = require('express');
 const router = express.Router();
 const jwtService = require('../services/jwt');
-const orderController = require('../controllers/order');
+const orderService = require('../services/order');
+const logService = require('../services/apiLog');
 
-router.post('/save', jwtService.verifyJWT, orderController.saveOrder);
-router.get('/find/customer/nuDocument=:nuDocument', jwtService.verifyJWT, orderController.findByNuDocument);
-router.get('/find/customer/idCustomer=:idCustomer', jwtService.verifyJWT, orderController.findByIdCustomer);
-router.put('/update/id=:id', jwtService.verifyJWT, orderController.updateOrder);
-router.delete('/delete/id=:id', jwtService.verifyJWT, orderController.cancelOrder);
+const saveOrder = async (req, res) => {
+
+  const dtStart = new Date().toJSON();
+
+  const response = await orderService.saveNewOrder(req);
+
+  await logService.saveLogDB(req, response, dtStart);
+
+  return res.status(response.statusCode).send(response.jsonBody);
+
+};
+
+const findByNuDocument = async (req, res) => {
+
+  const dtStart = new Date().toJSON();
+
+  const response = await orderService.findOrderByNuDocumentCustomer(req);
+
+  await logService.saveLogDB(req, response, dtStart);
+
+  return res.status(response.statusCode).send(response.jsonBody);
+
+};
+
+const findByIdCustomer = async (req, res) => {
+
+  const dtStart = new Date().toJSON();
+
+  const response = await orderService.findOrderByIdCustomer(req);
+
+  await logService.saveLogDB(req, response, dtStart);
+
+  return res.status(response.statusCode).send(response.jsonBody);
+
+};
+
+const updateOrder = async (req, res) => {
+
+  const dtStart = new Date().toJSON();
+
+  const response = await orderService.updateOrder(req);
+
+  await logService.saveLogDB(req, response, dtStart);
+
+  return res.status(response.statusCode).send(response.jsonBody);
+
+};
+
+const cancelOrder = async (req, res) => {
+
+  const dtStart = new Date().toJSON();
+
+  const response = await orderService.findAndCancelOrder(req);
+
+  await logService.saveLogDB(req, response, dtStart);
+
+  return res.status(response.statusCode).send(response.jsonBody);
+
+};
+
+router.post('/save', jwtService.verifyJWT, saveOrder);
+router.get('/find/customer/nuDocument=:nuDocument', jwtService.verifyJWT, findByNuDocument);
+router.get('/find/customer/idCustomer=:idCustomer', jwtService.verifyJWT, findByIdCustomer);
+router.put('/update/id=:id', jwtService.verifyJWT, updateOrder);
+router.delete('/delete/id=:id', jwtService.verifyJWT, cancelOrder);
 module.exports = router;
