5de95e753d1aa33a75fa707207069095a59ff67d
tglima <thg.limadesousa@gmail.com>
2022-11-26 12:17:00 -0300

    Remove o controller e refatora o route Atribui  as responsabilidades do controller para o route

--- app/routes/customer.js
+++ app/routes/customer.js
@@ -1,10 +1,59 @@
 const express = require('express');
 const router = express.Router();
-const customerController = require('../controllers/customer');
 const jwtService = require('../services/jwt');
+const customerService = require('../services/customer');
+const logService = require('../services/apiLog');
 
-router.post('/save', jwtService.verifyJWT, customerController.saveCustomer);
-router.put('/update/id=:id', jwtService.verifyJWT, customerController.updateCustomer);
-router.get('/find/id=:id', jwtService.verifyJWT, customerController.findById);
-router.get('/find/nuDocument=:nuDocument', jwtService.verifyJWT, customerController.findByNuDocument);
+const findById = async (req, res) => {
+
+  const dtStart = new Date().toJSON();
+
+  const response = await customerService.findById(req);
+
+  await logService.saveLogDB(req, response, dtStart);
+
+  return res.status(response.statusCode).send(response.jsonBody);
+
+};
+
+const findByNuDocument = async (req, res) => {
+
+  const dtStart = new Date().toJSON();
+
+  const response = await customerService.findByNuDocument(req);
+
+  await logService.saveLogDB(req, response, dtStart);
+
+  return res.status(response.statusCode).send(response.jsonBody);
+
+};
+
+const saveCustomer = async (req, res) => {
+
+  const dtStart = new Date().toJSON();
+
+  const response = await customerService.saveCustomer(req);
+
+  await logService.saveLogDB(req, response, dtStart);
+
+  return res.status(response.statusCode).send(response.jsonBody);
+
+};
+
+const updateCustomer = async (req, res) => {
+
+  const dtStart = new Date().toJSON();
+
+  const response = await customerService.updateCustomer(req);
+
+  await logService.saveLogDB(req, response, dtStart);
+
+  return res.status(response.statusCode).send(response.jsonBody);
+
+};
+
+router.post('/save', jwtService.verifyJWT, saveCustomer);
+router.put('/update/id=:id', jwtService.verifyJWT, updateCustomer);
+router.get('/find/id=:id', jwtService.verifyJWT, findById);
+router.get('/find/nuDocument=:nuDocument', jwtService.verifyJWT, findByNuDocument);
 module.exports = router;
