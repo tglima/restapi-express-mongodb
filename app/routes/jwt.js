6578e6fb0fb20d798af2af92881afb539d211e66
tglima <thg.limadesousa@gmail.com>
2022-11-26 12:08:22 -0300

    Remove o controller e refatora o route Atribui  as responsabilidades do controller para o route

--- app/routes/jwt.js
+++ app/routes/jwt.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const jwtController = require('../controllers/jwt');
+const jwtService = require('../services/jwt');
+const logService = require('../services/apiLog');
 
-router.post('/auth', jwtController.auth);
+const auth = async (req, res) => {
+
+  const dtStart = new Date().toJSON();
+  const response = await jwtService.checkAuthDb(req.body);
+  await logService.saveLogDB(req, response, dtStart);
+  return res.status(response.statusCode).send(response.jsonBody);
+
+};
+
+router.post('/auth', auth);
 module.exports = router;
