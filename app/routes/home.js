fdce87b787ed529717e8b0c827e3d24571a7e078
tglima <thg.limadesousa@gmail.com>
2022-11-26 12:06:17 -0300

    Remove o controller e refatora o route Atribui  as responsabilidades do controller para o route

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const homeController = require('../controllers/home');
+const logService = require('../services/apiLog');
+const constant = require('../helpers/constants');
 const router = express.Router();
 
-router.get('/', homeController.getHome);
-router.post('/', homeController.getHome);
-router.put('/', homeController.getHome);
-router.delete('/', homeController.getHome);
+const getHome = async (req, res) => {
+
+  const dtStart = new Date().toJSON();
+  const response = constant.RESULT_DEF_ERROR_404;
+  await logService.saveLogDB(req, response, dtStart);
+  return res.status(response.statusCode).send(response.jsonBody);
+
+};
+
+router.get('/', getHome);
+router.post('/', getHome);
+router.put('/', getHome);
+router.delete('/', getHome);
 module.exports = router;
