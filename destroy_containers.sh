82c0a0f9efe8ebf31cc5093922fd2e93544dcc9c
tglima <thg.limadesousa@gmail.com>
2022-12-19 20:47:55 -0300

    Remove o arquivos desnecessários

--- destroy_containers.sh
+++
@@ -1,8 +0,0 @@
- #!/bin/sh
-#Remove os containers e destroi a rede criada
-docker-compose down
-#Remove a imagem do container db
-docker rmi db
-#Remove a imagem do container app
-docker rmi app
-echo "Tarefa finalizada";
