b29f4144e1f78bfc56134b43231466b6e7a687ee
tglima <thg.limadesousa@gmail.com>
2022-12-19 20:59:49 -0300

    Melhoria no script

--- docs/shell_script/start_node.sh
+++ docs/shell_script/start_node.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
+#Variáveis auxiliares
+qtAttemp=0;
+prefix="DB_SERVER=";
+#Pega a URL do mongo que está configurada no arquivo .env
+urlDB=`cat .env | grep $prefix | sed "s/'//g" | sed "s/$prefix//g"`
+#Tenta se conectar ao mongo antes de tentar criar o container
+clear
+RESULT=''
+while [ "$RESULT" != "200" ]
+do
+  sleep 2;
+  RESULT=$(curl -o /dev/null -s -w "%{http_code}\n" $urlDB)
+  echo "Tentando se conectar ao mongodb";
+  qtAttemp=$((qtAttemp+1))
+  if [ $qtAttemp -ge 5 ]
+  then
+      echo "Não foi possível se conectar ao MongoDB!"
+      exit 1
+  fi
+done
+#Carrega o localtunnel
+node /usr/src/webapi/docs/nodejs_helper/load-localtunnel.js &
 #Caso exista o arquivo swagger.json removemos
-rm -f /usr/src/app/swagger.json
+rm -f /usr/src/webapi/swagger.json;
+#Agurda 15 segundos
+sleep 15
 #Criamos um novo arquivo swagger.json
-touch /usr/src/app/swagger.json
+touch /usr/src/webapi/swagger.json
 #Atualizamos o arquivos com a documentação do swagger
-node  /usr/src/app/swagger-generation.js
+node /usr/src/webapi/swagger-generation.js
 #Iniciamos o pm2 para iniciar e gerenciar nossa aplicação
-pm2-runtime start /usr/src/app/pm2.json
+pm2-runtime start /usr/src/webapi/pm2.json
