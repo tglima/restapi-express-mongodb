f6d48a99da65eaec523f82b5330170de6322bba8
tglima <thg.limadesousa@gmail.com>
2022-12-24 19:20:10 -0300

    Implementa melhorias Fixa a URL do subdomain Exibe mensagem de erro caso o localtunnel pare.

--- docs/nodejs_helper/load-localtunnel.js
+++ docs/nodejs_helper/load-localtunnel.js
@@ -1,14 +1,23 @@
+/* eslint-disable no-process-exit */
+/* eslint-disable no-console */
 const localtunnel = require('localtunnel');
 const helper = require('./helpers');
 require('dotenv').config();
 
 (async () => {
 
-  const tunnel = await localtunnel({ port: process.env.SERVER_PORT });
+  const tunnel = await localtunnel(
+    { port: process.env.SERVER_PORT, subdomain: process.env.URL_SUBDOMAIN },
+  );
+
   helper.setEnvValue('URL_DOMAIN', tunnel.url);
-  // eslint-disable-next-line no-console
-  console.log(`Link gerado: ${tunnel.url}`);
+  console.log(`Link gerado: ${helper.getUrlAPI()}`);
+
   tunnel.on('close', () => {
+
+    console.error('Error: localtunnel stoped!');
+    return process.exit(1);
+
   });
 
 })();
