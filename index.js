91e796d3f5b87c4c9849213bb1895cc2ac8fff20
tglima <thg.limadesousa@gmail.com>
2022-12-24 07:30:08 -0300

    Adiciona regra para ignorar o warning

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+/* eslint-disable node/no-unpublished-require */
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
