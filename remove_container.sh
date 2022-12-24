c3aa2514b056e11f3d5793615bc9f7ab88ecae42
tglima <thg.limadesousa@gmail.com>
2022-12-24 08:17:53 -0300

    Simplifica a destruição do container

--- remove_container.sh
+++ remove_container.sh
@@ -1,7 +1,4 @@
  #!/bin/sh
-#Remove os containers e destroi a rede criada
-docker-compose down
-#Remove a imagem do container db
-#Remove a imagem do container app
-docker rmi webapi
+#Remove os container, destroi a rede criada e a image gerada
+docker-compose down --rmi all -v
 echo "Tarefa de remoção de container finalizada";
