d01dbe51101796d68e235cd0e5f1e86832e72093
tglima <thg.limadesousa@gmail.com>
2023-02-12 18:36:45 -0300

    Adiciona o pacote express-rate-limit GH-9

--- index.js
+++ index.js
@@ -6,16 +6,25 @@ const app = express();
 const mongoose = require('mongoose');
 const swaggerUI = require('swagger-ui-express');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const appConfig = require('./app/config/app.config');
 const router = require('./app/routes/routes');
 const swaggerFile = require('./swagger.json');
 const helpers = require('./docs/nodejs_helper/helpers');
 const messageSwaggerUp = `Swagger disponível em: ${helpers.getUrlSwagger()}\n`;
 
+const apiLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 120,
+  statusCode: 429,
+  message: 'Limite de requisicoes ultrapassado, por favor, aguarde.',
+});
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(helmet.ieNoOpen());
 app.use(helmet.hidePoweredBy());
+app.use('/', apiLimiter);
 
 app.use(
   `${appConfig.pathSwagger}`,
