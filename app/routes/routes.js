92693878c5c18b1d5f12f1429ad8fd7e41f75bf1
tglima <thg.limadesousa@gmail.com>
2023-02-19 19:58:42 -0300

    Fix issue #7 Implementa a rota /api/v1/healthcheck

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -6,11 +6,13 @@ const customerRoutes = require('./customer');
 const productRoutes = require('./product');
 const orderRoutes = require('./order');
 const jwtRoutes = require('./jwt');
+const healthRoutes = require('./health');
 
 router.use('', jwtRoutes);
 router.use('/customer', customerRoutes);
 router.use('/product', productRoutes);
 router.use('/order', orderRoutes);
 router.use('/contact', contactMessageRoutes);
+router.use('', healthRoutes);
 router.use('**', defaultRoutes);
 module.exports = router;
