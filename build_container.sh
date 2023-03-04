0d5f75ac57a1282286fb03fecafd907f61f436c7
tglima <thg.limadesousa@gmail.com>
2023-03-04 15:48:39 -0300

    Melhoria na validação de conexão com o MongoDB

--- build_container.sh
+++ build_container.sh
@@ -31,12 +31,24 @@ fi
 prefix="DB_SERVER=";
 #Carrego a URL do mongo que está configurada no arquivo .env
 urlDB=`cat ./config/env/$nodeEnv.env | grep $prefix | sed "s/'//g" | sed "s/$prefix//g"`
-clear
+
+prefix="DB_PREFIX_CONN=";
+#Carrego o prefixo da conexão do mongo que está configurada no arquivo .env
+dbPrefixConn=`cat ./config/env/$nodeEnv.env | grep $prefix | sed "s/'//g" | sed "s/$prefix//g"`
+
+RESULT=''
+
+#Caso o início da var dbPrefixConn não seja mongodb significa que a conexão do banco pode
+#ser direto na MongoDB/AWS, sendo assim não temos como testar a conexão por aqui
+#desta forma já carregamos a var RESULT com o valor 200 para que ele pule o while
+if [ $dbPrefixConn != 'mongodb' ]
+then
+  RESULT='200'
+fi
 
 #Tenta 5x se conectar ao mongo antes de tentar criar o container
 #Caso o status retornado não seja 200 mesmo após as 5 tentativas, o processo é interrompido
 qtAttemp=0;
-RESULT=''
 while [ "$RESULT" != "200" ]
 do
   sleep 2;
@@ -45,7 +57,7 @@ do
   qtAttemp=$((qtAttemp+1))
   if [ $qtAttemp -ge 5 ]
   then
-      echo -e "Não foi possível se conectar ao MongoDB!";
+      echo "Não foi possível se conectar ao MongoDB!";
       exit 1
   fi
 done
--- docs/shell_script/start_node.sh
+++ docs/shell_script/start_node.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 #Variáveis auxiliares
-qtAttemp=0;
+nodeEnv=$NODE_ENV;
 
-#Carrega o valor das variáveis correspondente a config que esta em execução
 prefix="DB_SERVER=";
-nodeEnv="NODE_ENV=";
+#Carrego a URL do mongo que está configurada no arquivo .env
+urlDB=`cat ./config/env/$nodeEnv.env | grep $prefix | sed "s/'//g" | sed "s/$prefix//g"`
 
-#Filtra o arquivo .env correspondente a config que esta em execução e
-fileEnv=`printenv | grep $nodeEnv | sed -e 's/\"//g' | sed "s/$nodeEnv//g"`;
+prefix="DB_PREFIX_CONN=";
+#Carrego o prefixo da conexão do mongo que está configurada no arquivo .env
+dbPrefixConn=`cat ./config/env/$nodeEnv.env | grep $prefix | sed "s/'//g" | sed "s/$prefix//g"`
 
-#Pega a URL do mongo que está configurada no arquivo .env
-urlDB=`cat ./config/env/$fileEnv.env | grep $prefix | sed "s/'//g" | sed "s/$prefix//g"`
-
-#Tenta se conectar ao mongo antes de tentar criar o container
-clear
 RESULT=''
+
+#Caso o início da var dbPrefixConn não seja mongodb significa que a conexão do banco pode
+#ser direto na MongoDB/AWS, sendo assim não temos como testar a conexão por aqui
+#desta forma já carregamos a var RESULT com o valor 200 para que ele pule o while
+if [ $dbPrefixConn != 'mongodb' ]
+then
+  RESULT='200'
+fi
+
+#Tenta 5x se conectar ao mongo antes de tentar criar o container
+#Caso o status retornado não seja 200 mesmo após as 5 tentativas, o processo é interrompido
+qtAttemp=0;
 while [ "$RESULT" != "200" ]
 do
   sleep 2;
@@ -23,9 +31,10 @@ do
   qtAttemp=$((qtAttemp+1))
   if [ $qtAttemp -ge 5 ]
   then
-      echo "Não foi possível se conectar ao MongoDB!"
+      echo "Não foi possível se conectar ao MongoDB!";
       exit 1
   fi
 done
+clear
 #Iniciamos o pm2 para iniciar e gerenciar nossa aplicação
 pm2-runtime start /usr/src/webapi/pm2.json
