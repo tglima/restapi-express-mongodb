c964aa2e8fcf3d8ed690f3bfe7b437c3550e01f1
tglima <thg.limadesousa@gmail.com>
2024-05-24 07:44:17 -0300

    refact: Altera json de retorno da rota health-check

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,7 @@ const router = express.Router();
 
 router.get('/health-check', (req, res) => {
   res.status(200).json({
-    status: 'OK',
-    message: 'API IS UP!',
+    messages: ['OK'],
   });
 });
 
