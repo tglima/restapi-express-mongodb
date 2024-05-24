70282def3d4b4e9da7e43aa723cba7a19d8275b6
tglima <thg.limadesousa@gmail.com>
2024-05-24 07:10:47 -0300

    feat: Adiciona arquivos de mensagens

---
+++ src/messages/error-messages.js
@@ -0,0 +1,2 @@
+export const INTERNAL_SERVER_ERROR = 'Erro interno no servidor';
+export const RESOURCE_NOT_FOUND = 'Recurso não encontrado';
---
+++ src/messages/server-messages.js
@@ -0,0 +1,4 @@
+import { ENV_MODE, NU_PORT, URL_SWAGGER } from '../configs/config-manager';
+
+export const MSG_START_SERVER = `Env mode: ${ENV_MODE}\nAPI listening on ${NU_PORT}\nSwagger link: ${URL_SWAGGER}`;
+export const MSG_START_DB = 'Conexão com o banco de dados estabelecida com sucesso';
