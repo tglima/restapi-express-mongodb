f6b2d35283848497534e8f93892d69aa9076ffd9
tglima <thg.limadesousa@gmail.com>
2024-05-30 14:47:39 -0300

    feat: Implementa a class AppReposity

---
+++ src/database/repositories/mongodb/AppRepository.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-underscore-dangle */
+import { model } from 'mongoose';
+import AppDbSchema from '../../schemas/mongodb/AppDbSchema';
+import getAppConfig from './operations/appOperations';
+import AppException from '../../../entities/AppException';
+
+const appDbSchema = model('appModel', AppDbSchema, 'apps');
+
+class AppRepository {
+  async getAppConfigById(id) {
+    const appDB = await appDbSchema.findById({ _id: id });
+
+    if (!appDB) {
+      throw new AppException('', '');
+    }
+
+    return getAppConfig(appDB);
+  }
+}
+
+export default new AppRepository();
