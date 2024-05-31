e7d6624c20d052a242ab340c0e0dca15bcdff36b
tglima <thg.limadesousa@gmail.com>
2024-05-31 18:20:06 -0300

    fix: Altera o caminho do import

--- src/messages/server-messages.js
+++ src/messages/server-messages.js
@@ -1,4 +1,4 @@
-import { ENV_MODE, NU_PORT, URL_SWAGGER } from '../configs/config-manager';
+import { ENV_MODE, NU_PORT, URL_SWAGGER } from '../services/configServices';
 
 export const MSG_START_SERVER = `Env mode: ${ENV_MODE}\nAPI listening on ${NU_PORT}\nSwagger link: ${URL_SWAGGER}`;
 export const MSG_START_DB = 'Conexão com o banco de dados estabelecida com sucesso';
