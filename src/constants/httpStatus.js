a3b3b09816a95538230d7af44ae2a263e650cf06
tglima <thg.limadesousa@gmail.com>
2024-06-01 19:35:01 -0300

    feat: Adiciona novas constantes para serem usadas no projeto

---
+++ src/constants/httpStatus.js
@@ -0,0 +1,6 @@
+export const OK_STATUS = 200;
+export const CREATED_STATUS = 201;
+export const BAD_REQUEST_STATUS = 400;
+export const UNAUTHORIZED_STATUS = 401;
+export const NOT_FOUND_STATUS = 404;
+export const INTERNAL_SERVER_ERROR_STATUS = 500;
