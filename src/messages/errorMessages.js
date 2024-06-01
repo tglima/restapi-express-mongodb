4064da970d384ad8c0a276fad0b80b8e8ed7c85e
tglima <thg.limadesousa@gmail.com>
2024-06-01 19:31:07 -0300

    feat: Adiciona nova mensagem

--- src/messages/errorMessages.js
+++ src/messages/errorMessages.js
@@ -1,2 +1,3 @@
 export const INTERNAL_SERVER_ERROR = 'Erro interno no servidor';
 export const RESOURCE_NOT_FOUND = 'Recurso não encontrado';
+export const UNAUTHORIZED_MSG = 'Credenciais inválidas ou ausentes';
