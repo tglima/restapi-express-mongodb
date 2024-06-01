a9d76aa350a95ab42b7b49fbe9aab656e833f7e1
tglima <thg.limadesousa@gmail.com>
2024-06-01 19:38:11 -0300

    refact: Atualiza para utilizar constantes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import { RESOURCE_NOT_FOUND } from '../constants/errorMessages';
+import { NOT_FOUND_STATUS, OK_STATUS } from '../constants/httpStatus';
 
 const router = express.Router();
 
 router.get('/health-check', (req, res) => {
-  res.status(200).json({
+  res.status(OK_STATUS).json({
     messages: ['OK'],
   });
 });
 
 router.use((req, res, next) => {
-  res.status(404).json({ messages: [RESOURCE_NOT_FOUND] });
+  res.status(NOT_FOUND_STATUS).json({ messages: [RESOURCE_NOT_FOUND] });
 });
 
 export default router;
