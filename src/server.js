06be28517a273d3a43fe0167b565ae62f02daa3b
tglima <thg.limadesousa@gmail.com>
2024-06-02 17:16:17 -0300

    refact: Adiciona novos middlewares startLogRequest e checkAuth

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import helmet from 'helmet';
 import morganBody from 'morgan-body';
 import Youch from 'youch';
 import routes from './routes';
+import { checkAuth } from './services/validatorServices';
+import { startLogRequest } from './services/loggerServices';
 import { NU_PORT, TRUST_PROXY, TRUST_PROXY_VALUE, getDbConnection } from './services/configServices';
 import { INTERNAL_SERVER_ERROR } from './constants/errorMessages';
 import { MSG_START_SERVER } from './constants/serverMessages';
@@ -23,8 +25,10 @@ const exceptionHandler = () => {
 
 const middlewares = () => {
   server.use(express.json());
+  server.use(startLogRequest);
   server.use(helmet());
   server.set(TRUST_PROXY, TRUST_PROXY_VALUE);
+  server.use(checkAuth);
   server.use(routes);
   morganBody(server);
   exceptionHandler();
