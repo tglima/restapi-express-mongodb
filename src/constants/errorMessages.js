3d847050273f3543dcae20f745552f1a2ea0e0bc
tglima <thg.limadesousa@gmail.com>
2024-06-04 19:42:01 -0300

    feat: Adiciona nova constante APP_CONFIG_ERROR

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -1,3 +1,4 @@
 export const INTERNAL_SERVER_ERROR = 'Erro interno no servidor';
 export const RESOURCE_NOT_FOUND = 'Recurso não encontrado';
 export const UNAUTHORIZED_MSG = 'Credenciais inválidas ou ausentes';
+export const APP_CONFIG_ERROR = 'Ocorreu um erro ao carregar as configurações da aplicação';
