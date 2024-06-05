715a1e1ef487ed4e3312e44d3dbb10806220d384
tglima <thg.limadesousa@gmail.com>
2024-06-05 20:53:10 -0300

    refact: Passa a utilizar o método getCurrentDateTime

--- src/entities/LogEvent.js
+++ src/entities/LogEvent.js
@@ -1,16 +1,16 @@
-import moment from 'moment';
+import { getCurrentDateTime } from '../helpers';
 
 class LogEvent {
   constructor(nm_method) {
     this.nm_method = nm_method;
-    this.dt_start = moment().toISOString();
+    this.dt_start = getCurrentDateTime();
     this.dt_finish = null;
     this.was_error = false;
     this.messages = [];
   }
 
   setDtFinish() {
-    this.dt_finish = moment().toISOString();
+    this.dt_finish = getCurrentDateTime();
   }
 }
 
