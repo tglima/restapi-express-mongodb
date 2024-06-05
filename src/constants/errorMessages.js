31868f8f91fdde896883a7dcc45f02f589b6b033
tglima <thg.limadesousa@gmail.com>
2024-06-05 19:40:28 -0300

    feat: Adiciona nova constante BAD_REQUEST

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -1,4 +1,5 @@
 export const INTERNAL_SERVER_ERROR = 'Erro interno no servidor';
 export const RESOURCE_NOT_FOUND = 'Recurso não encontrado';
 export const UNAUTHORIZED_MSG = 'Credenciais inválidas ou ausentes';
+export const BAD_REQUEST = 'Ocorreu uma falha na sua requisição. Reveja os dados enviados e tente novamente!';
 export const APP_CONFIG_ERROR = 'Ocorreu um erro ao carregar as configurações da aplicação';
