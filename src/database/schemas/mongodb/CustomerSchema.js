84959be938114412cf1bbe0f1294a21d3e7d8c3d
tglima <thg.limadesousa@gmail.com>
2024-06-05 20:49:22 -0300

    feat: Implementa a CustomerSchema

---
+++ src/database/schemas/mongodb/CustomerSchema.js
@@ -0,0 +1,21 @@
+import { Schema } from 'mongoose';
+import { getCurrentDateTime } from '../../../helpers';
+
+const CustomerSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    gender: { type: String, enum: ['F', 'M'], required: true },
+    dt_birth: { type: Date, required: true },
+    nu_document: { type: String, required: true, unique: true },
+    email: { type: String },
+    nu_phone: { type: String },
+    user_register: { type: String, required: true },
+    last_user_edit: { type: String, required: true },
+    dt_register: { type: Date, default: getCurrentDateTime() },
+    dt_last_edit: { type: Date, default: getCurrentDateTime() },
+    is_active: { type: Boolean, default: true },
+  },
+  { versionKey: false }
+);
+
+export default CustomerSchema;
