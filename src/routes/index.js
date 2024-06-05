a41fee3e063d8427fec46be97fb184676779b229
tglima <thg.limadesousa@gmail.com>
2024-06-05 20:54:19 -0300

    feat: Implementa a rota get customers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import ResponseAPI from '../entities/ResponseApi';
 import productFind from '../controllers/productController';
+import { find as customerFind } from '../controllers/customerController';
 import { saveLogRequest } from '../services/loggerServices';
 import { RESOURCE_NOT_FOUND } from '../constants/errorMessages';
 import { NOT_FOUND_STATUS, OK_STATUS } from '../constants/httpStatus';
@@ -16,6 +17,8 @@ router.get('/health-check', (req, res) => {
 router.get('/products', async (req, res) => {
   await productFind(req, res);
 });
+
+router.get('/customers', customerFind);
 
 router.use((req, res, next) => {
   const bodyResponse = new ResponseAPI(req.LogRequest.request_id, [RESOURCE_NOT_FOUND]);
