7a7dc26cac7977573ce8b0a2dbbc3a6c9576c542
tglima <thg.limadesousa@gmail.com>
2024-06-06 20:00:24 -0300

    refact: Implementa o método toSchema

--- src/database/repositories/mongodb/mappers/productMapper.js
+++ src/database/repositories/mongodb/mappers/productMapper.js
@@ -1,4 +1,4 @@
-export default function toEntity(productSchema) {
+export const toEntity = (productSchema) => {
   return {
     id_product: productSchema.id_product,
     nm_product: productSchema.nm_product,
@@ -7,4 +7,15 @@ export default function toEntity(productSchema) {
     nm_resolution: productSchema.nm_resolution,
     qt_simultaneous_screens: productSchema.qt_simultaneous_screens,
   };
-}
+};
+
+export const toSchema = (product) => {
+  return {
+    id_product: product.id_product,
+    nm_product: product.nm_product,
+    vl_month_price: product.vl_month_price,
+    nm_videoQuality: product.nm_videoQuality,
+    nm_resolution: product.nm_resolution,
+    qt_simultaneous_screens: product.qt_simultaneous_screens,
+  };
+};
