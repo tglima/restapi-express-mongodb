4ac4d25090ab965d851d3f0e6f6a256891237abc
tglima <thg.limadesousa@gmail.com>
2024-06-06 19:37:20 -0300

    feat: Implementa OrderSchema

---
+++ src/database/schemas/mongodb/OrderDbSchema.js
@@ -0,0 +1,17 @@
+import { Schema } from 'mongoose';
+import { getCurrentDateTime } from '../../../helpers';
+import ProductSchema from './ProductSchema';
+
+const OrderSchema = new Schema({
+  id_customer: { type: String, required: true },
+  product: ProductSchema,
+  dt_register: { type: Date, default: getCurrentDateTime() },
+  dt_last_edit: { type: Date, default: getCurrentDateTime() },
+  user_register: { type: String, required: true },
+  last_user_edit: { type: String, required: true },
+  dtStart: { type: Date, default: getCurrentDateTime() },
+  dtFinish: { type: Date, required: true },
+  is_active: { type: Boolean, default: true },
+});
+
+export default OrderSchema;
