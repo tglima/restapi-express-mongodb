476613feffa3ee7125c4deb3dcec3503759cd637
tglima <thg.limadesousa@gmail.com>
2024-06-16 14:05:21 -0300

    refact: Alterar para salvar os logs em caso de erro, e utilza o mesmo padrão de retorno em caso de erros

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-console */
 import express from 'express';
+import 'express-async-errors';
 import helmet from 'helmet';
 import morganBody from 'morgan-body';
 import Youch from 'youch';
 import routes from './routes';
 import { checkAuth } from './services/validatorServices';
-import { startLogRequest } from './services/loggerServices';
+import { saveLogRequest, startLogRequest } from './services/loggerServices';
 import { NU_PORT, TRUST_PROXY, TRUST_PROXY_VALUE, getDbConnection } from './services/configServices';
 import { INTERNAL_SERVER_ERROR } from './constants/errorMessages';
 import { MSG_START_SERVER } from './constants/serverMessages';
@@ -13,13 +14,24 @@ import { MSG_START_SERVER } from './constants/serverMessages';
 const server = express();
 
 const exceptionHandler = () => {
-  server.use(async (err, request, response, next) => {
-    const errors = await new Youch(err, request).toJSON();
-    console.error(errors);
+  server.use(async (err, req, res, next) => {
+    if (err) {
+      const youch = new Youch(err, req);
+      const jsonError = await youch.toJSON();
 
-    console.error({ method: 'exceptionHandler', error: err }).then(() => {
-      return response.status(500).json({ messages: [INTERNAL_SERVER_ERROR] });
-    });
+      console.error(JSON.stringify(jsonError));
+
+      req.LogRequest.status_code = 500;
+      req.LogRequest.output_resquest = {
+        request_id: req.LogRequest.request_id,
+        messages: [INTERNAL_SERVER_ERROR],
+      };
+
+      await saveLogRequest(req, res);
+      res.status(req.LogRequest.status_code).json(req.LogRequest.output_resquest);
+    } else {
+      next();
+    }
   });
 };
 
