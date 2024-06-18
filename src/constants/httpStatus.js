fc512c59740ace458b5d5cbde2f65204c7d2a6d3
tglima <thg.limadesousa@gmail.com>
2024-06-17 21:11:34 -0300

    refact: Remove arquivo por não ser mais necessário

--- src/constants/httpStatus.js
+++
@@ -1,6 +0,0 @@
-export const OK_STATUS = 200;
-export const CREATED_STATUS = 201;
-export const BAD_REQUEST_STATUS = 400;
-export const UNAUTHORIZED_STATUS = 401;
-export const NOT_FOUND_STATUS = 404;
-export const INTERNAL_SERVER_ERROR_STATUS = 500;
